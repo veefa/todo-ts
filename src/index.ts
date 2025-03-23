b96706c50bf3288568708f5b78c716d4ca18c507
vefa <44091688+veefa@users.noreply.github.com>
2025-03-23 16:55:25 +0000

    ,

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,38 @@ import inquirer from "inquirer";
 import fs from "fs";
 import chalk from "chalk";
 
-interface Tesk {
+// File to store tasks
+const filePath = "tasks.json"
+
+// Task type
+interface Task {
     id: number,
     text: string,
     completed: boolean
-}
+};
+
+// Function to load tasks from a file
+const loadTasks = (): Task[] => {
+    try {
+        const data = fs.readFileSync(filePath, "utf-8");
+        return JSON.parse(data);
+      } catch {
+        return [];
+      }
+};
+
+// Function to save tasks to a file
+const saveTasks = (tasks: Task[]) => {
+    fs.writeFileSync(filePath, JSON.stringify(tasks, null, 2));
+};
+
+// Function to display tasks
+const showTasks = () => {
+    const tasks = loadTasks();
+    console.log("\nYour todo List:");
+    tasks.forEach(task =>{console.log(`${task.id}. ${task.completed ? "[✔]" : "[ ]"} ${task.text}`);
+    });
+};
+
+// Function to add a new task
+const addTasks = () => {}
