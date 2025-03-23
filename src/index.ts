4a98fa57d95c11380d5e7a1ae7b0505414333b61
vefa <44091688+veefa@users.noreply.github.com>
2025-03-23 17:12:57 +0000

    .

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import inquirer from "inquirer";
 import fs from "fs";
 import chalk from "chalk";
+import { text } from "stream/consumers";
 
 // File to store tasks
 const filePath = "tasks.json"
@@ -36,4 +37,44 @@ const showTasks = () => {
 };
 
 // Function to add a new task
-const addTasks = () => {}
+const addTasks = async () => {
+    const answer = await inquirer.prompt({
+        type: "input",
+        name: "text",
+        message: "Enter a new task:",
+    });
+    const tasks = loadTasks();
+    const newTask = {
+        id: tasks.length + 1,
+        text: answer.text,
+        completed: false,
+    };
+    tasks.push(newTask);
+    saveTasks(tasks);
+
+    console.log("Task added! ");
+};
+
+// Function to mark a task as done
+const markAsDone = async () => {
+    const tasks = loadTasks();
+    if (tasks.length === 0) {
+        console.log("No tasks available.")
+        return;
+    }
+
+    const answer = await inquirer.prompt({
+        type: "number",
+        name: "id",
+        message: "Enter task ID to mark as done:",
+    });
+
+    const task = tasks.find(t => t.id === answer.id);
+    if (task) {
+    task.completed = true;
+    saveTasks(tasks);
+    console.log("Task marked as done!");
+    } else {
+    console.log("Invalid task ID.");
+    }
+};
